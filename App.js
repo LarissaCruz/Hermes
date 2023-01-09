e2c53f300cc3913c2e37f61b7cd9d246e3320ed7
LarissaCruz <61520705+LarissaCruz@users.noreply.github.com>
2023-01-09 18:02:50 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/navigation/StackNavigator";
+import { useEffect } from "react";
+import { criarTabela } from "./src/service/storageSQLite/clientes";
 
 export default function App() {
+  useEffect(()=>{
+    criarTabela();
+  },[])
+
   return (
     <>
       <StatusBar backgroundColor={"black"} barStyle="default" hidden={false} />
